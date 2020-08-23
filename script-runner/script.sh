966b5f3af132b6bd9fc5c3bacd2309e0fe9ada75
Alvaro Garcia Jaen <alvaro@alvaro.network>
2020-08-23 20:44:33 +0200

    New approach for script-runner container

--- script-runner/script.sh
+++ script-runner/script.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-mkdir ssl
-cd ssl
-../scripts/create-cert.sh
+# Run all scripts
+/volume/scripts/create-cert.sh
+/volume/scripts/generate-provision-scripts.sh
 
-cd ../scripts
-generate-provision-scripts.sh
+mkdir -p /volume/storage/config-provider/ssl
+mkdir -p /volume/storage/config-provider/scripts
+cp config-provider.crt /volume/storage/config-provider/ssl
+cp config-provider.key /volume/storage/config-provider/ssl
+cp provision-client.sh /volume/storage/config-provider/scripts/client.sh
+cp provision-server.sh /volume/storage/config-provider/scripts/server.sh
+
+mkdir -p /volume/storage/elasticsearch/ssl
+cp node01.key /volume/storage/elasticsearch/ssl/node01.key
+cp node01.crt /volume/storage/elasticsearch/ssl/node01.crt
+cp ca.crt /volume/storage/elasticsearch/ssl/ca.crt
+
+mkdir -p /volume/storage/logstash/ssl
+cp ca.crt /volume/storage/logstash/ssl/ca.crt
+
+mkdir -p /volume/storage/kibana/ssl
+cp ca.crt /volume/storage/kibana/ssl/ca.crt
+cp kibana.key /volume/storage/kibana/ssl/kibana.key
+cp kibana.crt /volume/storage/kibana/ssl/kibana.crt
