87703c2083400012017a5f87520c84fd87dbba62
Alvaro Garcia Jaen <alvaro@alvaro.network>
2020-08-28 22:19:04 +0200

    Modify script for making it more verbose

--- scripts/wait-for-script-runner.sh
+++ scripts/wait-for-script-runner.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 set -e
+host=$1
+shift
+callback=$@
 
-until nc -z $1 1234; do
+until nc -z $host 1234; do
+  echo Waiting for $host to finsh;
   sleep 1;
 done
 
 shift
-exec $@
+exec $callback
