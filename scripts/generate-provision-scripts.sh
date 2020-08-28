c11f3be12edf5c08e9d765a68348632f65ad6d45
Alvaro Garcia Jaen <alvaro@alvaro.network>
2020-08-28 22:19:48 +0200

    Fix script that generate the provision scripts

--- scripts/generate-provision-scripts.sh
+++ scripts/generate-provision-scripts.sh
@@ -4,16 +4,18 @@
 echo "#!/bin/bash" > provision-client.sh
 echo "sudo cp /etc/rsyslog.conf /etc/rsyslog.conf.bak" >> provision-client.sh
 echo "sudo tee /etc/rsyslog.conf > /dev/null <<EOL" >> provision-client.sh
-cat ../rsyslog/rsyslog-client.conf >> provision-client.sh
+cat $1/rsyslog-client.conf >> provision-client.sh
 echo "EOL" >> provision-client.sh
 echo "sudo ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime" >> provision-client.sh
+echo "sudo systemctl restart rsyslog" >> provision-client.sh
 chmod +x provision-client.sh
 
 # SERVER
 echo "#!/bin/bash" > provision-server.sh
 echo "sudo cp /etc/rsyslog.conf /etc/rsyslog.conf.bak" >> provision-server.sh
 echo "sudo tee /etc/rsyslog.conf > /dev/null <<EOL" >> provision-server.sh
-cat ../rsyslog/rsyslog-server.conf >> provision-server.sh
+cat $1/rsyslog-server.conf >> provision-server.sh
 echo "EOL" >> provision-server.sh
 echo "sudo ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime" >> provision-server.sh
+echo "sudo systemctl restart rsyslog" >> provision-server.sh
 chmod +x provision-server.sh
